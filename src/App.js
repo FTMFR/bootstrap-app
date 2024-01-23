8b8d6178435a73bca035e8ab823a48730be59d75
FTMFR <fatemefarajzade018@gmail.com>
2024-01-23 17:03:29 +0330

    add until here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import Menu from './component/Menus/Menu';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Menu />
+        <Col></Col>
+        <Col></Col>
+      </Row>
+    </Container>
   );
 }
 
